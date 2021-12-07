24140b0df327ae0eee71ec95ee3f73c7ace6a3ac
ycshang <351234359@qq.com>
2021-12-07 16:42:09 +0800

    配置跨域问题

---
+++ myblog/src/main/java/top/ycshang/myblog/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package top.ycshang.myblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: myblog
+ * @description: 跨域配置类
+ * @author: ycshang
+ * @create: 2021-12-07 16:41
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
