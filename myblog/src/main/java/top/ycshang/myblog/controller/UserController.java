d7bc8ac827068e634f9826653eb937734f9eb513
ycshang <351234359@qq.com>
2021-12-07 14:58:56 +0800

    后端API接口测试通过

--- myblog/src/main/java/top/ycshang/myblog/controller/UserController.java
+++ myblog/src/main/java/top/ycshang/myblog/controller/UserController.java
@@ -1,20 +1,33 @@
 package top.ycshang.myblog.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+import top.ycshang.myblog.service.UserService;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author ycshang
  * @since 2021-12-07
  */
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Resource
+    private UserService userService;
 
+    @GetMapping("/{id}")
+    public Object test(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
 }
+
+
